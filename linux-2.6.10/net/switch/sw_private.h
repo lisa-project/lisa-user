ea7def570992720039fdc472f5651b160fd1a6fd
Radu Rendec <radu.rendec@ines.ro>
2005-03-13 19:00:46 +0000

    Port list, synchronization & stuff

--- linux-2.6.10/net/switch/sw_private.h
+++ linux-2.6.10/net/switch/sw_private.h
@@ -1,7 +1,24 @@
 #ifndef _SW_PRIVATE_H
 #define _SW_PRIVATE_H
 
+#include <linux/netdevice.h>
+#include <linux/list.h>
+#include <asm/semaphore.h>
+
+struct net_switch {
+	/* List of all ports in the switch */
+	struct list_head ports;
+
+	/* We don't want an interface to be added or removed "twice". This
+	   would mess up the usage counter, the promiscuity counter and many
+	   other things.
+	 */
+	struct semaphore adddelif_mutex;
+};
+
 struct net_switch_port {
+	struct list_head lh;
+	struct net_device *dev;
 };
 
 #endif
--- linux-2.6.10/net/switch/switch.c
+++ linux-2.6.10/net/switch/switch.c
@@ -2,6 +2,7 @@
 #include <linux/module.h>
 #include <linux/switch.h>
 #include <linux/netdevice.h>
+#include <linux/slab.h>
 #include <asm/semaphore.h>
 
 #include "sw_private.h"
@@ -19,42 +20,55 @@ MODULE_VERSION("0.1");
 
 extern int (*sw_handle_frame_hook)(struct net_switch_port *p, struct sk_buff **pskb);
 
-/* We don't want an interface to be added or removed "twice". This
-   would mess up the usage counter, the promiscuity counter and many
-   other things.
- */
-DECLARE_MUTEX(adddelif_mutex);
+static struct net_switch sw;
+
+static void init_switch(struct net_switch *sw) {
+	INIT_LIST_HEAD(&sw->ports);
+	init_MUTEX(&sw->adddelif_mutex);
+}
 
 static int sw_addif(struct net_device *dev) {
-	if(down_interruptible(&adddelif_mutex))
+	struct net_switch_port *port;
+	if(down_interruptible(&sw.adddelif_mutex))
 		return -EINTR;
 	if(rcu_dereference(dev->sw_port) != NULL) {
 		/* dev->sw_port shouldn't be changed elsewhere, so
 		   we don't necessarily need rcu_dereference here
 		 */
-		up(&adddelif_mutex);
+		up(&sw.adddelif_mutex);
 		return -EBUSY;
 	}
-	rcu_assign_pointer(dev->sw_port, 1);
-	up(&adddelif_mutex);
+	if((port = kmalloc(sizeof(struct net_switch_port), GFP_KERNEL)) == NULL)
+		return -ENOMEM;
+	port->dev = dev;
+	list_add_tail(&port->lh, &sw.ports);
+	rcu_assign_pointer(dev->sw_port, port);
 	dev_hold(dev);
 	dev_set_promiscuity(dev, 1);
 	dbg("Added device %s to switch\n", dev->name);
+	up(&sw.adddelif_mutex);
 	return 0;
 }
 
-static int sw_delif(struct net_device *dev) {
-	if(down_interruptible(&adddelif_mutex))
-		return -EINTR;
-	if(rcu_dereference(dev->sw_port) == NULL) {
-		up(&adddelif_mutex);
-		return -ENODEV;
-	}
+static void __sw_delif(struct net_device *dev, struct net_switch_port *port) {
 	rcu_assign_pointer(dev->sw_port, NULL);
-	up(&adddelif_mutex);
+	list_del(&port->lh);
+	kfree(port);
 	dev_put(dev);
 	dev_set_promiscuity(dev, -1);
 	dbg("Removed device %s from switch\n", dev->name);
+}
+
+static int sw_delif(struct net_device *dev) {
+	struct net_switch_port *port;
+	if(down_interruptible(&sw.adddelif_mutex))
+		return -EINTR;
+	if((port = rcu_dereference(dev->sw_port)) == NULL) {
+		up(&sw.adddelif_mutex);
+		return -ENODEV;
+	}
+	__sw_delif(dev, port);
+	up(&sw.adddelif_mutex);
 	return 0;
 }
 
@@ -94,6 +108,7 @@ static int sw_handle_frame(struct net_switch_port *port, struct sk_buff **pskb) 
 }
 
 static int switch_init(void) {
+	init_switch(&sw);
 	swioctl_set(sw_deviceless_ioctl);
 	sw_handle_frame_hook = sw_handle_frame;
 	dbg("Switch module initialized\n");
@@ -101,6 +116,19 @@ static int switch_init(void) {
 }
 
 static void switch_exit(void) {
+	struct list_head *pos, *n;
+	struct net_switch_port *port;
+	/* Remove all interfaces from switch */
+	down_interruptible(&sw.adddelif_mutex);
+	list_for_each_safe(pos, n, &sw.ports) {
+		port = list_entry(pos, struct net_switch_port, lh);
+		__sw_delif(port->dev, port);
+	}
+	up(&sw.adddelif_mutex);
+	/* Interfaces cannot be added now although the lock is released. 
+	   To add an interface one must do an ioctl, which calls
+	   request_module(). This fails if the module is unloading.
+	 */
 	swioctl_set(NULL);
 	sw_handle_frame_hook = NULL;
 	dbg("Switch module unloaded\n");
