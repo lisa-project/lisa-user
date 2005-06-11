8f605f5a9d0a49f51ae7b90d34418c33445d4fcb
Ionut Nicu <ionut.nicu@ines.ro>
2005-06-11 16:36:58 +0000

    Adaugat header cu GPL

--- userspace/cli/climain.h
+++ userspace/cli/climain.h
@@ -1,3 +1,22 @@
+/*
+ *    This file is part of LiSA Command Line Interface.
+ *
+ *    LiSA Command Line Interface is free software; you can redistribute it 
+ *    and/or modify it under the terms of the GNU General Public License 
+ *    as published by the Free Software Foundation; either version 2 of the 
+ *    License, or (at your option) any later version.
+ *
+ *    LiSA Command Line Interface is distributed in the hope that it will be 
+ *    useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with LiSA Command Line Interface; if not, write to the Free 
+ *    Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, 
+ *    MA  02111-1307  USA
+ */
+
 #ifndef _CLIMAIN_H
 #define _CLIMAIN_H
 
