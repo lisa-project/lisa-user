e69043dd0fb9689fc2562260d431d87ada508216
Radu Rendec <radu.rendec@ines.ro>
2006-03-02 20:40:07 +0000

    removed some junk files

--- linux-2.6.15/net/switch/.built-in.o.cmd
+++
@@ -1,1 +0,0 @@
-cmd_net/switch/built-in.o :=  rm -f net/switch/built-in.o; ar rcs net/switch/built-in.o
--- linux-2.6.15/net/switch/.switch.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_net/switch/switch.ko := ld -m elf_i386 -m elf_i386 -r -o net/switch/switch.ko net/switch/switch.o net/switch/switch.mod.o
