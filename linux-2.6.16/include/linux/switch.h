e953754afbd9957739e75b0d6553d91b489c324e
Ionut Nicu <ionut.nicu@ines.ro>
2007-09-30 19:20:58 +0300

    Structure changes for lisa.git repository
    * Removed linux-2.6.x dirs
    
    * Patched kernel will be a git submodule. This repository
    is cloned from Linus Torvald's linux-2.6 tree and has
    branches for each lisa patch. The branches are split
    from release tags (i.e. branch lisa_2.6.21 is split from
    the v2.6.21 tag)
    
    * We don't support 2.6.10 anymore, because the first linux
    version in git history is 2.6.11.
    
    * Userspace Makefile assumes the kernel is the main project
    dir and has the name linux-2.6
    
    * Added tags for lisa releases. Tag names are identical to
    branch names from the linux-2.6 submodule.
    
    Signed-off-by: Ionut Nicu <ionut.nicu@ines.ro>

--- linux-2.6.16/include/linux/switch.h
+++
@@ -1,6 +0,0 @@
-#ifndef _SWITCH_H
-#define _SWITCH_H
-
-extern void swioctl_set(int (*)(unsigned int, void __user *));
-
-#endif
--- linux-2.6.19/net/switch/test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-swctl add eth3
-swctl add eth4
-swctl setportvlan eth3 11
-swctl addportvlan eth4 11
-swctl settrunk eth4 1
-swctl addvlan 11 teste
