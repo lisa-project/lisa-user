eec36024474bc268625dce1689551221e31052b3
Radu Rendec <radu.rendec@ines.ro>
2007-10-20 22:19:00 +0300

    Shm manager now compiles.
    Signed-off-by: Radu Rendec <radu.rendec@ines.ro>

--- userspace/include/mm.h
+++ userspace/include/mm.h
@@ -20,57 +20,202 @@
 #ifndef _MM_H
 #define _MM_H
 
-typedef mm_ptr unsigned int;
-typedef mm_dptr unsigned int;
+#include <semaphore.h>
+
+typedef unsigned int mm_ptr_t;
+#define MM_NULL 0
 
 /* mm managed area handle */
 struct mm_private {
 	int fd;
 	sem_t *sem;
+	int lock;
 	char *area;
-	char *dynamic_area;
-}
+	size_t mapped_size;
+};
 
 /* kernel style list for mm */
 struct mm_list_head {
-	mm_ptr prev, next;
-}
+	mm_ptr_t prev, next;
+};
 
 /* main descriptor of shared memory area */
 struct mm_shared {
 	size_t static_size;
 	size_t dynamic_size;
 	struct mm_list_head lh;
-}
+};
 
 struct mm_chunk {
 	/* chunk usable size (w/o sizeof(struct mm_chunk)) */
-	unsigned int size;
-	int free;
+	size_t size;
 	struct mm_list_head lh;
+};
+
+static __inline__ void *mm_addr(struct mm_private *mm, mm_ptr_t p)
+{
+	return (void *)(mm->area + p);
 }
 
-static __inline__ void *mm_addr(struct mm_private *mm, mm_ptr p)
+static __inline__ mm_ptr_t mm_ptr(struct mm_private *mm, void *a)
 {
-	return (void *)(mm->dynamic_area + p);
+	return (char *)a - mm->area;
 }
 
-#define MM_INIT_LIST_HEAD(mm, ptr) do { \
-	(struct mm_list_head *)mm_addr(mm, ptr)->next = (ptr); \
-	(struct mm_list_head *)mm_addr(mm, ptr)->prev = (ptr); \
-} while (0)
-
-#define mm_list_entry(ptr, type, member) \
-	((mm_ptr)((ptr) - (unsigned long)(&((type *)0)->member)))
-
-static __inline__ void mm_lock(struct mm_private *mm)
+static __inline__ void __mm_lock(struct mm_private *mm)
 {
+	if (mm->lock++)
+		return;
 	sem_wait(mm->sem);
 }
 
+#define MM_INIT_LIST_HEAD(mm, ptr) do { \
+	MM_LHPTR(mm, ptr)->next = (ptr); \
+	MM_LHPTR(mm, ptr)->prev = (ptr); \
+} while (0)
+
+#define mm_list_entry(ptr, type, member) \
+	((mm_ptr_t)((ptr) - (unsigned long)(&((type *)0)->member)))
+
+#define MM_LHPTR(mm, p) ((struct mm_list_head *)mm_addr((mm), (p)))
+
+extern void mm_lock(struct mm_private *);
+
 static __inline__ void mm_unlock(struct mm_private *mm)
 {
+	if (--mm->lock)
+		return;
 	sem_post(mm->sem);
 }
+
+/*
+ * Insert a new entry between two known consecutive entries. 
+ *
+ * This is only for internal list manipulation where we know
+ * the prev/next entries already!
+ */
+static __inline__ void __mm_list_add(struct mm_private *mm, mm_ptr_t new,
+		mm_ptr_t prev, mm_ptr_t next)
+{
+	MM_LHPTR(mm, next)->prev = new;
+	MM_LHPTR(mm, new)->next = next;
+	MM_LHPTR(mm, new)->prev = prev;
+	MM_LHPTR(mm, prev)->next = new;
+}
+
+/**
+ * list_add - add a new entry
+ * @new: new entry to be added
+ * @head: list head to add it after
+ *
+ * Insert a new entry after the specified head.
+ * This is good for implementing stacks.
+ */
+static __inline__ void mm_list_add(struct mm_private *mm, mm_ptr_t new,
+		mm_ptr_t head)
+{
+	__mm_list_add(mm, new, head, MM_LHPTR(mm, head)->next);
+}
+
+/**
+ * list_add_tail - add a new entry
+ * @new: new entry to be added
+ * @head: list head to add it before
+ *
+ * Insert a new entry before the specified head.
+ * This is useful for implementing queues.
+ */
+static __inline__ void mm_list_add_tail(struct mm_private *mm, mm_ptr_t new,
+		mm_ptr_t head)
+{
+	__mm_list_add(mm, new, MM_LHPTR(mm, head)->prev, head);
+}
+
+/*
+ * Delete a list entry by making the prev/next entries
+ * point to each other.
+ *
+ * This is only for internal list manipulation where we know
+ * the prev/next entries already!
+ */
+static __inline__ void __mm_list_del(struct mm_private *mm, mm_ptr_t prev,
+		mm_ptr_t next)
+{
+	MM_LHPTR(mm, next)->prev = prev;
+	MM_LHPTR(mm, prev)->next = next;
+}
+
+/**
+ * list_del - deletes entry from list.
+ * @entry: the element to delete from the list.
+ * Note: list_empty on entry does not return true after this, the entry is in an undefined state.
+ */
+static __inline__ void mm_list_del(struct mm_private *mm, mm_ptr_t entry)
+{
+	__mm_list_del(mm, MM_LHPTR(mm, entry)->prev, MM_LHPTR(mm, entry)->next);
+	MM_LHPTR(mm, entry)->next = MM_LHPTR(mm, entry)->prev = 0;
+}
+
+/**
+ * list_del_init - deletes entry from list and reinitialize it.
+ * @entry: the element to delete from the list.
+ */
+static __inline__ void mm_list_del_init(struct mm_private *mm, mm_ptr_t entry)
+{
+	__mm_list_del(mm, MM_LHPTR(mm, entry)->prev, MM_LHPTR(mm, entry)->next);
+	MM_INIT_LIST_HEAD(mm, entry); 
+}
+
+/**
+ * list_empty - tests whether a list is empty
+ * @head: the list to test.
+ */
+static __inline__ int mm_list_empty(struct mm_private *mm, mm_ptr_t head)
+{
+	return MM_LHPTR(mm, head)->next == head;
+}
+
+/**
+ * list_for_each	-	iterate over a list
+ * @pos:	the &struct list_head to use as a loop counter.
+ * @head:	the head for your list.
+ */
+#define mm_list_for_each(mm, pos, head) \
+	for (pos = MM_LHPTR(mm, head)->next; pos != (head); \
+        	pos = MM_LHPTR(mm, pos)->next)
+
+/**
+ * list_for_each_safe	-	iterate over a list safe against removal of list entry
+ * @pos:	the &struct list_head to use as a loop counter.
+ * @n:		another &struct list_head to use as temporary storage
+ * @head:	the head for your list.
+ */
+#define mm_list_for_each_safe(mm, pos, n, head) \
+	for (pos = MM_LHPTR(mm, head)->next, n = MM_LHPTR(mm, pos)->next; pos != (head); \
+		pos = n, n = MM_LHPTR(mm, pos)->next)
+
+/**
+ * list_for_each_entry	-	iterate over list of given type
+ * @pos:	the type * to use as a loop counter.
+ * @head:	the head for your list.
+ * @member:	the name of the list_struct within the struct.
+ */
+#define mm_list_for_each_entry(mm, pos, head, member)				\
+	for (pos = mm_addr(mm, mm_list_entry(MM_LHPTR(mm, head)->next, typeof(*pos), member));	\
+	     &pos->member != mm_addr(mm, head); 		\
+	     pos = mm_addr(mm, mm_list_entry(pos->member.next, typeof(*pos), member)))
+
+/**
+ * list_for_each_entry_safe - iterate over list of given type safe against removal of list entry
+ * @pos:	the type * to use as a loop counter.
+ * @n:		another type * to use as temporary storage
+ * @head:	the head for your list.
+ * @member:	the name of the list_struct within the struct.
+ */
+#define mm_list_for_each_entry_safe(pos, n, head, member)			\
+	for (pos = mm_addr(mm, mm_list_entry(MM_LHPTR(mm, head)->next, typeof(*pos), member)),	\
+		n = mm_addr(mm, mm_list_entry(pos->member.next, typeof(*pos), member));	\
+	     &pos->member != mm_addr(mm, head); 					\
+	     pos = n, n = mm_addr(mm, mm_list_entry(pos->member.next, typeof(*pos), member)))
 
 #endif
--- userspace/lib/mm.c
+++ userspace/lib/mm.c
@@ -17,22 +17,46 @@
  *    MA  02111-1307  USA
  */
 
-struct mm_private *mm_create(const char *name, size_t static_size, size_t dynamic_size) {
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <string.h>
+
+#include "mm.h"
+
+static __inline__ void *mm_map(struct mm_private *mm)
+{
+	return mmap(NULL, mm->mapped_size, PROT_READ | PROT_WRITE, MAP_SHARED, mm->fd, 0);
+}
+
+static __inline__ size_t page_size(size_t size)
+{
+	size_t p_size = sysconf(_SC_PAGE_SIZE);
+	return p_size * ((size + p_size - 1) / p_size);
+}
+
+struct mm_private *mm_create(const char *name, size_t static_size, size_t dynamic_size)
+{
 	struct mm_private *ret;
-	int unlock = 0;
+	int unlock = 0, unlink = 0; /* cleanup flags */
 	void *mm_area;
 
 	ret = malloc(sizeof(struct mm_private));
 	if (NULL == ret)
 		return NULL;
-	ret->fd = -1;
 
+	ret->fd = -1;
+	ret->lock = 0;
 	do {
 		ret->sem = sem_open(name, O_CREAT, 0600, 1);
 		if (SEM_FAILED == ret->sem)
 			break;
 
-		mm_lock(ret);
+		__mm_lock(ret);
 		unlock = 1;
 		ret->fd = shm_open(name, O_CREAT | O_EXCL | O_RDWR, 0600);
 
@@ -43,22 +67,192 @@ struct mm_private *mm_create(const char *name, size_t static_size, size_t dynami
 			/* already there, open without O_EXCL and go; if
 			 * something goes wrong, close and cleanup */
 			ret->fd = shm_open(name, O_RDWR, 0600);
+			ret->mapped_size = sizeof(struct mm_shared);
 			if (-1 == ret->fd)
 				break;
-			mm_area = mmap(NULL, sizeof(struct mm_shared), PROT_READ | PROT_WRITE, ret->fd, 0);
+			mm_area = mm_map(ret);
 			if (NULL == mm_area)
+				break;
+			ret->mapped_size = sizeof(struct mm_shared) +
+				((struct mm_shared *)mm_area)->static_size +
+				((struct mm_shared *)mm_area)->dynamic_size;
+			munmap(mm_area, sizeof(struct mm_shared));
+			mm_area = mm_map(ret);
+			if (NULL == mm_area)
+				break;
 		} else {
 			/* we just created the shm area, must setup; if
 			 * something goes wrong, delete the shm area and
 			 * cleanup (don't leave half-initialized garbage) */
+			ret->mapped_size = page_size(static_size + dynamic_size + sizeof(struct mm_shared));
+
+			unlink = 1;
+
+			if (-1 == ftruncate(ret->fd, ret->mapped_size))
+				break;
+			mm_area = mm_map(ret);
+			if (NULL == mm_area)
+				break;
+
+			/* initialize shared data and dynamic allocator */
+			((struct mm_shared *)mm_area)->static_size = static_size;
+			((struct mm_shared *)mm_area)->dynamic_size = ret->mapped_size -
+				(sizeof(struct mm_shared) + static_size);
+			MM_INIT_LIST_HEAD(ret, mm_ptr(ret, &((struct mm_shared *)mm_area)->lh));
 		}
 
-
 		mm_unlock(ret);
+		return ret;
 	} while(0);
+
+	/* cleanup */
+	if (-1 != ret->fd)
+		close(ret->fd);
+
+	if (unlink)
+		shm_unlink(name);
 
 	if (unlock)
 		mm_unlock(ret);
 	
 	free(ret);
 	return NULL;
+}
+
+/* Lock the associated semaphore and make sure the shm area is
+ * properly mapped (i.e. the shm size has not changed)
+ */
+void mm_lock(struct mm_private *mm)
+{
+	size_t mm_size;
+
+	if (mm->lock++)
+		return;
+	sem_wait(mm->sem);
+
+	mm_size = sizeof(struct mm_shared) +
+		((struct mm_shared *)mm->area)->static_size +
+		((struct mm_shared *)mm->area)->dynamic_size;
+ 
+	if (mm_size <= mm->mapped_size)
+		return;
+
+	munmap(mm->area, mm->mapped_size);
+	mm->mapped_size = mm_size;
+	mm->area = mm_map(mm);
+}
+
+/* Extend the mm area by size bytes */
+int mm_extend(struct mm_private *mm, size_t size)
+{
+	size_t mm_size;
+	if (!size)
+		return 0;
+
+	mm_lock(mm);
+	mm_size = mm->mapped_size + size;
+	if (-1 == ftruncate(mm->fd, mm_size)) {
+		mm_unlock(mm);
+		return 1;
+	}
+
+	munmap(mm->area, mm->mapped_size);
+	mm->mapped_size = mm_size;
+	mm->area = mm_map(mm);
+
+	mm_unlock(mm);
+	return 0;
+}
+
+mm_ptr_t mm_alloc(struct mm_private *mm, size_t size)
+{
+	mm_ptr_t base, last;
+	size_t real_size;
+	struct mm_chunk *chunk;
+	struct mm_shared *shr;
+
+	if (!size)
+		return MM_NULL;
+
+	mm_lock(mm);
+	shr = (struct mm_shared *)mm->area;
+	last = base = sizeof(struct mm_shared) + shr->static_size;
+	real_size = sizeof(struct mm_chunk) + size;
+
+	do {
+		/* first see if there is no allocated chunk yet */
+		if (mm_list_empty(mm, mm_ptr(mm, &shr->lh)))
+			break;
+
+		/* find first gap that can accomodate */
+		mm_list_for_each_entry(mm, chunk, mm_ptr(mm, &shr->lh), lh) {
+			if (mm_ptr(mm, chunk) - last < real_size) {
+				last = mm_ptr(mm, chunk) + chunk->size;
+				continue;
+			}
+			chunk = mm_addr(mm, last);
+			chunk->size = real_size;
+			__mm_list_add(mm, mm_ptr(mm, &chunk->lh), chunk->lh.prev, chunk->lh.next);
+			mm_unlock(mm);
+			return last + sizeof(struct mm_chunk);
+		}
+	
+		/* allocate between the last chunk and the end of the dynamic area;
+		 * last is already set up properly */
+	} while (0);
+
+	if (base + shr->dynamic_size < last + real_size)
+		if (mm_extend(mm, (last + real_size) - (base + shr->dynamic_size))) {
+			mm_unlock(mm);
+			return MM_NULL;
+		}
+	chunk = mm_addr(mm, last);
+	chunk->size = real_size;
+	mm_list_add_tail(mm, mm_ptr(mm, &chunk->lh), mm_ptr(mm, &shr->lh));
+	mm_unlock(mm);
+	return last + sizeof(struct mm_chunk);
+	/* don't use mm_ptr() on chunk, because we know it's at last ;) */
+}
+
+void mm_free(struct mm_private *mm, mm_ptr_t ptr)
+{
+	if (MM_NULL == ptr)
+		return;
+	mm_lock(mm);
+	mm_list_del(mm, mm_ptr(mm, &((struct mm_chunk *)mm_addr(mm, ptr - sizeof(struct mm_chunk)))->lh));
+	mm_unlock(mm);
+}
+
+mm_ptr_t mm_realloc(struct mm_private *mm, mm_ptr_t ptr, size_t size)
+{
+	struct mm_chunk *chunk;
+	size_t real_size;
+	mm_ptr_t new;
+
+	if (MM_NULL == ptr)
+		return mm_alloc(mm, size);
+	if (!size) {
+		mm_free(mm, ptr);
+		return MM_NULL;
+	}
+
+	mm_lock(mm);
+	chunk = mm_addr(mm, ptr - sizeof(struct mm_chunk));
+	real_size = size + sizeof(struct mm_chunk);
+
+	if (chunk->size >= real_size) {
+		chunk->size = real_size;
+		mm_unlock(mm);
+		return ptr;
+	}
+
+	new = mm_alloc(mm, size);
+	if (MM_NULL == new) {
+		mm_unlock(mm);
+		return MM_NULL;
+	}
+	memcpy(mm_addr(mm, new), mm_addr(mm, ptr), chunk->size);
+	mm_free(mm, ptr);
+	mm_unlock(mm);
+	return new;
+}
