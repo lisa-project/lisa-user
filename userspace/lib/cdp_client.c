7b39220fb24378228b34e38bd1cd044558020c44
Ionut Nicu <ionut.nicu@ines.ro>
2009-03-07 22:31:04 +0200

    Moved cdp global configuration into shared memory
    * Both cdpd and cli instances must use the shared memory
    to get cdp global configuration parameters
    * Moved the cdp configuration and traffic stats structures
    from cdpd.h to cdp_client.h
    * Removed the ipc commands to get/set global configuration
    parameters
    * Added the CDP_SESSION_OPEN, CDP_SESSION_CLOSE macros
    * Updated the cdp_client library
    
    Signed-off-by: Ionut Nicu <ionut.nicu@ines.ro>

--- userspace/lib/cdp_client.c
+++ userspace/lib/cdp_client.c
@@ -5,11 +5,12 @@
 #include "cdp_client.h"
 
 /* Initiates a cdp client session */
-int cdp_session_start(struct cdp_session *s) {
+struct cdp_session *cdp_session_start(void) {
+	struct cdp_session *s;
 	struct mq_attr attr;
-	int    status = 0;
 
 	/* Be paranoid about user input */
+	s = (struct cdp_session *)malloc(sizeof(struct cdp_session));
 	assert(s);
 	memset(s, 0, sizeof(struct cdp_session));
 
@@ -19,7 +20,7 @@ int cdp_session_start(struct cdp_session *s) {
 	s->sq_name[sizeof(s->sq_name)-1] = '\0';
 
 	if ((s->sq = mq_open(s->sq_name, O_WRONLY)) < 0)
-		return s->sq;
+		goto out_err;
 
 	/* Open the message queue for receiving responses from cdpd */
 	memset(s->rq_name, 0, sizeof(s->rq_name));
@@ -27,32 +28,31 @@ int cdp_session_start(struct cdp_session *s) {
 	s->rq_name[sizeof(s->rq_name)-1] = '\0';
 
 	if ((s->rq = mq_open(s->rq_name, O_CREAT|O_RDONLY, 0666, NULL)) < 0)
-		return s->rq;
+		goto out_err;
 
 	/* Get the max message size attribute */
-	if ((status = mq_getattr(s->rq, &attr)) < 0)
-		return status;
+	if (mq_getattr(s->rq, &attr) < 0)
+		goto out_err;
 
 	s->max_msg_len = attr.mq_msgsize;
 
 	/* Alloc the receive message buffer */
-	if (!(s->cdp_response = malloc(s->max_msg_len)))
-		return -ENOMEM;
+	if (!(s->response = malloc(s->max_msg_len)))
+		goto out_err;
 
-	s->enabled = 1;
+	return s;
 
-	return status;
+out_err:
+	cdp_session_end(s);
+	return NULL;
 }
 
 /* Ends a cdp client session */
 void cdp_session_end(struct cdp_session *s) {
 	assert(s);
 
-	if (!s->enabled)
-		return;
-
-	if (s->cdp_response)
-		free(s->cdp_response);
+	if (s->response)
+		free(s->response);
 	mq_close(s->rq);
 	mq_unlink(s->rq_name);
 
@@ -68,13 +68,12 @@ int cdp_session_recv(struct cdp_session *s) {
 
 	memset(&ts, 0, sizeof(ts));
 	time(&ns);
-	ts.tv_sec = ns + CDP_CLIENT_TIMEOUT_S;
+	ts.tv_sec = ns + CDP_CLIENT_TIMEOUT;
 
-	if (mq_timedreceive(s->rq, s->cdp_response,  s->max_msg_len, NULL, &ts) < 0) {
+	if (mq_timedreceive(s->rq, s->response,  s->max_msg_len, NULL, &ts) < 0) {
 		fprintf(stderr, "No message received from cdpd in %d seconds.\n"
-			"Disabling cdp in this session.\n", CDP_CLIENT_TIMEOUT_S);
-		s->enabled = 0;
-		return 1;
+			"Disabling cdp in this session.\n", CDP_CLIENT_TIMEOUT);
+		return -EAGAIN;
 	}
 
 	return 0;
