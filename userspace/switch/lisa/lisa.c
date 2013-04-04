68c09e76ef4bb08f5ccbe077937cc19e9e714aed
Andreea Hodea <hodea_andreea@yahoo.com>
2013-04-04 08:13:36 +0000

    Implement vlan_add for LiSA middleware

--- userspace/switch/lisa/lisa.c
+++ userspace/switch/lisa/lisa.c
@@ -31,9 +31,35 @@ static int vlan_rename(struct switch_operations *sw_ops, int vlan, char *desc)
 	return rc;
 }
 
+static int vlan_add(struct switch_operations *sw_ops, int vlan)
+{
+	struct swcfgreq swcfgr;
+	int rc, sock_fd, ioctl_errno;
+	struct lisa_context *uc = SWLiSA_CTX(sw_ops);
+
+	swcfgr.vlan = vlan;
+	swcfgr.cmd = SWCFG_ADDVLAN;
+
+	SW_SOCK_OPEN(uc, sock_fd);
+	rc = ioctl(sock_fd, SIOCSWCFG, &swcfgr);
+	ioctl_errno = errno;
+	SW_SOCK_CLOSE(uc, sock_fd); /* this can overwrite ioctl errno */
+
+	if (rc == -1 && ioctl_errno == EACCES)
+		rc = -EACCES;
+
+	if (ioctl_errno == EEXIST)
+		rc = -EEXIST;
+
+	sw_ops->sw_errno = rc;
+	return rc;
+}
+
 /* TODO implement switch API with lisa module */
 struct lisa_context lisa_ctx = {
+	.sock_fd = -1,
 	.sw_ops = (struct switch_operations) {
+		.vlan_add = vlan_add,
 		.vlan_rename = vlan_rename,
 		.get_vlan_desc = get_vlan_desc
 	}
--- userspace/switch/lisa/lisa.h
+++ userspace/switch/lisa/lisa.h
@@ -1,7 +1,32 @@
 #ifndef _LISA_H
 #define _LISA_H
 
+#include <unistd.h>
+
+#include <linux/sockios.h>
+
 #include "sw_api.h"
+#include "swsock.h"
+
+#define SWLiSA_CTX(sw_ops) ((struct lisa_context *)(sw_ops))
+
+#define SW_SOCK_OPEN(__ctx, __sock_fd) do {\
+	if (__ctx->sock_fd != -1) {\
+		__sock_fd = __ctx->sock_fd;\
+		break;\
+	}\
+	__sock_fd = socket(PF_SWITCH, SOCK_RAW, 0); \
+	if (__sock_fd == -1) {\
+		__ctx->sw_ops.sw_errno = errno; \
+		return errno; \
+	}\
+} while(0)
+
+#define SW_SOCK_CLOSE(__ctx, __sock_fd) do {\
+	if (__sock_fd != __ctx->sock_fd)\
+		close(__sock_fd);\
+} while (0)
+
 
 struct lisa_context {
 
@@ -10,5 +35,7 @@ struct lisa_context {
 	/* Socket descriptor used for communication with LiSA module. */
 	int sock_fd;
 };
+
+extern struct lisa_context lisa_ctx;
 
 #endif
