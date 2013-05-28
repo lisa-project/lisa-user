477c0100b534ad7f1caab4186be835d05e844fc8
Radu Rendec <radu.rendec@cloudbit.ro>
2013-05-28 12:09:36 +0300

    Include libswitch.so dir in fix-ld-library-path.sh
    Change the fix-ld-library-path.sh script to also include the "switch"
    directory in LD_LIBRARY_PATH, so libswitch.so may be properly loaded.
    
    Signed-off-by: Radu Rendec <radu.rendec@cloudbit.ro>

--- userspace/cli/fix-ld-library-path.sh
+++ userspace/cli/fix-ld-library-path.sh
@@ -1,2 +1,8 @@
-real_path="`readlink -f -n "$PWD"`"
-export LD_LIBRARY_PATH="$real_path"
+#!/bin/bash
+# Interpreter line above is just for propper vim syntax highlighting.
+# You should always run this as ". fix-ld-library-path.sh" to get the
+# variables set in the current shell.
+
+real_path="$(readlink -f -n "$PWD")"
+switch_path="$(readlink -f -n "$real_path/../switch")"
+export LD_LIBRARY_PATH="$real_path:$switch_path"
