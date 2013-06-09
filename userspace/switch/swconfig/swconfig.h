5db11cf93fa54de9fe2260e67cc155dc724b12f5
Ion Dumitrache <gear@ines.ro>
2013-06-09 20:11:49 +0300

    [openwrt] swlib helper functions, issue #94
    Signed-off-by: Ion Dumitrache <gear@ines.ro>

--- userspace/switch/swconfig/swconfig.h
+++ userspace/switch/swconfig/swconfig.h
@@ -32,6 +32,7 @@ struct switch_dev {
 
 struct swconfig_context {
 	struct switch_operations sw_ops;
+	struct switch_dev *dev;
 	/* Hardware Switch id: based on HW sw driver register order */
 	int switch_id;
 	char *dev_name;		/* switch0 */
@@ -48,52 +49,82 @@ struct swconfig_context sw_ctx;
 #define	SWLIB_UNTAGGED	1
 #define	SWLIB_TAGGED	0
 
-/*** Global swconfig Switch Functions ***/
+/*** util_swlib.c functions ***/
 
 /* activate changes in HW switch */
-int switch_apply(struct swconfig_context *ctx);
+extern int switch_apply(struct swconfig_context *ctx);
 
 /* reset hw switch */
-int switch_reset(struct swconfig_context *ctx);
+extern int switch_reset(struct swconfig_context *ctx);
 
 /* enable switch VLAN mode */
-int switch_enable_vlans(struct swconfig_context *ctx); 
+extern int switch_enable_vlans(struct swconfig_context *ctx); 
 /* disable switch VLAN mode */
-int switch_disable_vlans(struct swconfig_context *ctx);
+extern int switch_disable_vlans(struct swconfig_context *ctx);
 
 
 /*** VLAN Switch Functions ***/
 
-/* get VLAN port assignments */
-int get_vlan_ports(int vlan, struct swconfig_context *ctx, int *ports);
+/* get/set VLAN-> port assignments */
+extern int get_vlan_ports(int vlan, struct swconfig_context *ctx, int *count, struct switch_port **ports );
+extern int set_vlan_ports(int vlan, struct swconfig_context *ctx, int count, struct switch_port **ports);
 
 
 /*** Switch Port Functions ***/
 
 /* enable/diable switch port */
-int port_enable(int id, struct swconfig_context *ctx);
-int port_disable(int id, struct swconfig_context *ctx);
+extern int port_enable(int id, struct swconfig_context *ctx);
+extern int port_disable(int id, struct swconfig_context *ctx);
 
 /* set/get switch port untagged */
-int port_set_untag(int untag, int id, struct swconfig_context *ctx);
-int port_get_untag(int id, struct swconfig_context *ctx);
+extern int port_set_untag(int id, struct swconfig_context *ctx, int untag);
+extern int port_get_untag(int id, struct swconfig_context *ctx, int *untag);
+
+
+/* set/get switch port primary vlan id */
+extern int port_set_pvid(int id, sw_ctx *ctx, int vlan);
+extern int port_get_pvid(int id, sw_ctx *ctx, int *vlan);
 
 /* set/get switch port doubletagging */
-int port_set_doubletag(int id, struct swconfig_context *ctx);
-int port_get_doubletag(int id, struct swconfig_context *ctx);
+extern int port_set_doubletag(int id, struct swconfig_context *ctx);
+extern int port_get_doubletag(int id, struct swconfig_context *ctx);
 
 /* set/get switch port HW group: 1 = lan, 0 = wan */
-int port_set_hwgroup(int lan, int id, struct swconfig_context *ctx);
-int port_get_hwgroup(int lan, int id, struct swconfig_context *ctx);
+extern int port_set_hwgroup(int id, struct swconfig_context *ctx, int lan);
+extern int port_get_hwgroup(int id, struct swconfig_context *ctx, int lan);
 
 /* get switch port link information */
-int port_get_link(int id, char *info, struct swconfig_context *ctx);
+extern int port_get_link(int id, struct swconfig_context *ctx, char *info);
 
 /* get switch port receive good packet count */
-int port_get_recv_good(int id, struct swconfig_context *ctx);
+extern int port_get_recv_good(int id, struct swconfig_context *ctx, int *count);
 /* get switch port receive bad packet count */
-int port_get_recv_bad(int id, struct swconfig_context *ctx);
+extern int port_get_recv_bad(int id, struct swconfig_context *ctx, int *count);
 
 
 
 #endif
+
+/*
+ * swconfig SYNTAX: 
+
+switch0: rt305x(rt305x-esw), ports: 7 (cpu @ 6), vlans: 4096
+     --switch
+        Attribute 1 (int): enable_vlan (VLAN mode (1:enabled))
+        Attribute 2 (int): alternate_vlan_disable (Use en_vlan instead of doubletag to disable VLAN mode)
+        Attribute 3 (none): apply (Activate changes in the hardware)
+        Attribute 4 (none): reset (Reset the switch)
+     --vlan
+        Attribute 1 (ports): ports (VLAN port mapping)
+     --port
+        Attribute 1 (int): disable (Port state (1:disabled))
+        Attribute 2 (int): doubletag (Double tagging for incoming vlan packets (1:enabled))
+        Attribute 3 (int): untag (Untag (1:strip outgoing vlan tag))
+        Attribute 4 (int): led (LED mode (0:link, 1:100m, 2:duplex, 3:activity, 4:collision, 5:linkact, 6:duplcoll, 7:10mact, 8:100mact, 10:blink, 12:on))
+        Attribute 5 (int): lan (HW port group (0:wan, 1:lan))
+        Attribute 6 (int): recv_bad (Receive bad packet counter)
+        Attribute 7 (int): recv_good (Receive good packet counter)
+        Attribute 8 (int): pvid (Primary VLAN ID)
+        Attribute 9 (string): link (Get port link information)
+
+ */
--- userspace/switch/swconfig/util_swlib.c
+++ userspace/switch/swconfig/util_swlib.c
@@ -23,30 +23,82 @@
 
 /* activate changes in HW switch */
 int switch_apply(sw_ctx *ctx){
+	int ret = 0;
+	struct switch_attr *a;
+	if(NULL == sw_ctx->dev) return -1;
 
+	a = swlib_lookup_attr(sw_ctx->dev, SWLIB_ATTR_GROUP_GLOBAL, "apply");
+	if(!a)	/* attribute unknown */
+		return -1;
+	ret = swlib_set_attr_string(sw_ctx->dev, a, -1, NULL);
+	return ret;
 }
 
 /* reset hw switch */
 int switch_reset(sw_ctx *ctx){
-
+	int ret = 0;
+	struct switch_attr *a;
+	if(NULL == sw_ctx->dev) return -1;
+	a = swlib_lookup_attr(sw_ctx->dev, SWLIB_ATTR_GROUP_GLOBAL, "reset");
+	if(!a)	/* attribute unknown */
+		return -1;
+	ret = swlib_set_attr_string(sw_ctx->dev, a, -1, NULL);
+	return ret;
 }
 
 /* enable switch VLAN mode */
 int switch_enable_vlans(sw_ctx *ctx){
+	int ret = 0;
+	struct switch_attr *a;
+	if(NULL == sw_ctx->dev) return -1;
 
+	a = swlib_lookup_attr(sw_ctx->dev, SWLIB_ATTR_GROUP_GLOBAL, "enable_vlan");
+	if(!a)	/* attribute unknown */
+		return -1;
+	ret = swlib_set_attr_string(sw_ctx->dev, a, -1, 1);
+	return ret;
 } 
 
 /* disable switch VLAN mode */
 int switch_disable_vlans(sw_ctx *ctx){
+	int ret = 0;
+	struct switch_attr *a;
+	if(NULL == sw_ctx->dev) return -1;
+
+	a = swlib_lookup_attr(sw_ctx->dev, SWLIB_ATTR_GROUP_GLOBAL, "enable_vlan");
+	if(!a)	/* attribute unknown */
+		return -1;
+	ret = swlib_set_attr_string(sw_ctx->dev, a, -1, 0);
+	return ret;
 
 }
 
 
 /*** VLAN Switch Functions ***/
 
-/* get VLAN->Port assignments */
-int get_vlan_ports(int vlan, sw_ctx *ctx, int *ports){
+/* set/get VLAN->Port assignments */
 
+int set_vlan_ports(int vlan, sw_ctx *ctx, int count, struct switch_port **ports){
+	return ret;
+}
+
+int get_vlan_ports(int vlan, sw_ctx *ctx, int *count, struct switch_port **ports){
+	int ret = 0;
+	struct switch_attr *a;
+	struct sitch_val val;
+	if(NULL == sw_ctx->dev) return -1;
+
+	ports = NULL;
+	val.port_vlan = vlan;
+	a = swlib_lookup_attr(sw_ctx->dev, SWLIB_ATTR_GROUP_VLAN, "ports");
+	if(!a)	/* attribute unknown */
+		return -1;
+	if((ret = swlib_get_attr(sw_ctx->dev, a, &val))<0)
+		goto out;
+	count = val.len;
+	ports = val.value.ports;
+out:
+	return ret;
 }
 
 
@@ -54,53 +106,160 @@ int get_vlan_ports(int vlan, sw_ctx *ctx, int *ports){
 
 /* enable/disable switch port */
 int port_enable(int id, sw_ctx *ctx){
+	int ret = 0;
+	struct switch_attr *a;
+	if(NULL == sw_ctx->dev) return -1;
 
+	a = swlib_lookup_attr(sw_ctx->dev, SWLIB_ATTR_GROUP_PORT, "disable");
+	if(!a)	/* attribute unknown */
+		return -1;
+	ret = swlib_set_attr_string(sw_ctx->dev, a, id, 1);
+	return ret;
 }
 
 int port_disable(int id, sw_ctx *ctx){
+	int ret = 0;
+	struct switch_attr *a;
+	if(NULL == sw_ctx->dev) return -1;
 
+	a = swlib_lookup_attr(sw_ctx->dev, SWLIB_ATTR_GROUP_PORT, "disable");
+	if(!a)	/* attribute unknown */
+		return -1;
+	ret = swlib_set_attr_string(sw_ctx->dev, a, id, 0);
+	return ret;
 }
 
 /* set/get switch port untagged */
-int port_set_untag(int untag, int id, sw_ctx *ctx){
+int port_set_untag(int id, sw_ctx *ctx, int untag){
+	int ret = 0;
+	struct switch_attr *a;
+	if(NULL == sw_ctx->dev) return -1;
 
+	a = swlib_lookup_attr(sw_ctx->dev, SWLIB_ATTR_GROUP_PORT, "untag");
+	if(!a)	/* attribute unknown */
+		return -1;
+	ret = swlib_set_attr_string(sw_ctx->dev, a, id, untag);
+	return ret;
 }
 
-int port_get_untag(int id, sw_ctx *ctx){
+int port_get_untag(int id, sw_ctx *ctx, int *untag){
+	int ret = 0;
+	struct switch_attr *a;
+	struct sitch_val val;
+	if(NULL == sw_ctx->dev) return -1;
 
+	*untag = -1;
+	val.port_vlan = id;
+	a = swlib_lookup_attr(sw_ctx->dev, SWLIB_ATTR_GROUP_PORT, "untag");
+	if(!a)	/* attribute unknown */
+		return -1;
+	ret = swlib_get_attr(sw_ctx->dev, a, &val);
+	if(!ret) 
+		untag = val.value.i;
+	return ret;
 }
 
+/* set/get switch port pvid (native vlan) */
+int port_set_pvid(int id, sw_ctx *ctx, int vlan){
+	int ret = 0;
+	struct switch_attr *a;
+	if(NULL == sw_ctx->dev) return -1;
+
+	a = swlib_lookup_attr(sw_ctx->dev, SWLIB_ATTR_GROUP_PORT, "pvid");
+	if(!a)	/* attribute unknown */
+		return -1;
+	ret = swlib_set_attr_string(sw_ctx->dev, a, id, vlan);
+	return ret;
+}
+
+int port_get_pvid(int id, sw_ctx *ctx, int *vlan){
+	int ret = 0;
+	struct switch_attr *a;
+	struct sitch_val val;
+	if(NULL == sw_ctx->dev) return -1;
+
+	*vlan = -1;
+	val.port_vlan = id;
+	a = swlib_lookup_attr(sw_ctx->dev, SWLIB_ATTR_GROUP_PORT, "pvid");
+	if(!a)	/* attribute unknown */
+		return -1;
+	ret = swlib_get_attr(sw_ctx->dev, a, &val);
+	if(!ret) 
+		vlan= val.value.i;
+	return ret;
+}
 
 /* set/get switch port doubletagging */
 int port_set_doubletag(int id, sw_ctx *ctx){
-
+	return 0;
 }
 
 int port_get_doubletag(int id, sw_ctx *ctx){
-
+	return 0;
 }
 
 /* set/get switch port HW group: 1 = lan, 0 = wan */
-int port_set_hwgroup(int lan, int id, sw_ctx *ctx){
-
+int port_set_hwgroup(int id, sw_ctx *ctx, int lan){
+	return 0;
 }
 
-int port_get_hwgroup(int lan, int id, sw_ctx *ctx){
-
+int port_get_hwgroup(int id, sw_ctx *ctx, int lan){
+	lan = 1 // lan;
+	return 0; 
 }
 
 /* get switch port link information */
-int port_get_link(int id, char *info, sw_ctx *ctx){
+int port_get_link(int id, char *info, sw_ctx *ctx, char *info){
+	int ret = 0;
+	struct switch_attr *a;
+	struct switch_val val;
+	if(NULL == sw_ctx->dev) return -1;
 
+	info = NULL;
+	val.port_vlan = id;
+	a = swlib_lookup_attr(sw_ctx->dev, SWLIB_ATTR_GROUP_PORT, "link");
+	if(!a)	/* attribute unknown */
+		return -1;
+	ret = swlib_get_attr(sw_ctx->dev, a, &val);
+	if(!ret) 
+		info = val.value.s;
+	return ret;
 }
 
 /* get switch port receive good packet count */
-int port_get_recv_good(int id, sw_ctx *ctx){
+int port_get_recv_good(int id, sw_ctx *ctx, int *count){
+	int ret = 0;
+	struct switch_attr *a;
+	struct sitch_val val;
+	if(NULL == sw_ctx->dev) return -1;
 
+	*count = 0;
+	val.port_vlan = id;
+	a = swlib_lookup_attr(sw_ctx->dev, SWLIB_ATTR_GROUP_PORT, "recv_good");
+	if(!a)	/* attribute unknown */
+		return -1;
+	ret = swlib_get_attr(sw_ctx->dev, a, &val);
+	if(!ret) 
+		untag = val.value.i;
+	return ret;
 }
-/* get switch port receive bad packet count */
-int port_get_recv_bad(int id, sw_ctx *ctx){
 
+/* get switch port receive bad packet count */
+int port_get_recv_bad(int id, sw_ctx *ctx, int *count){
+	int ret = 0;
+	struct switch_attr *a;
+	struct sitch_val val;
+	if(NULL == sw_ctx->dev) return -1;
+
+	*count = 0;
+	val.port_vlan = id;
+	a = swlib_lookup_attr(sw_ctx->dev, SWLIB_ATTR_GROUP_PORT, "recv_bad");
+	if(!a)	/* attribute unknown */
+		return -1;
+	ret = swlib_get_attr(sw_ctx->dev, a, &val);
+	if(!ret) 
+		untag = val.value.i;
+	return ret;
 }
 
 
